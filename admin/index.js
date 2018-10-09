a674eb7f2dd83abd3b07657b442730b43c07c375
Archit Garg <architgarg1997@gmail.com>
2018-10-09 21:05:31 +0530

    added admin module

---
+++ admin/index.js
@@ -0,0 +1,31 @@
+const error = $('.error')[0];
+
+function postNewVideo(url) {
+    $.ajax({
+        url: 'https://us-central1-faad-motivation.cloudfunctions.net/api/videos',
+        type: 'POST',
+        data: {url: url},
+        success: function (data) {
+            setErrorMsg('');
+            $('#url').val('');
+        },
+        error: function (error) {
+            setErrorMsg('Server Internal Error');
+        }
+    });
+}
+
+function validateUrl() {
+    const regex = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|&v(?:i)?=))([^#&?]*).*/;
+    const url = $('#url').val();
+
+    if (!url.match(regex)) {
+        setErrorMsg('Please post a valid a URL');
+    } else {
+        postNewVideo(url);
+    }
+}
+
+function setErrorMsg(msg) {
+    $('.error')[0].innerHTML = msg;
+}
